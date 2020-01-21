4b73db21a0bfea8b6081587cd02fbef9e23c6c2a
github username <joemokhtarali@gmail.com>
2020-01-20 21:36:54 -0500

    Done.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "Student" do 
   let!(:student) { Student.new }
-  
+   
   describe "#hello" do 
     it "returns a greeting phrase" do 
     expect{student.hello}.to output("Hey there! I'm so excited to learn stuff.\n").to_stdout
